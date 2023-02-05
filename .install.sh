0decddb588d35f9b9bd8c6a3d1bf582dbeb8fc72
Michael Jarvis <michael@jarvis.com>
2023-02-05 12:10:37 -0600

    add install script for testing

---
+++ .install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+DEST="${HOME}/Zomboid/mods"
+
+if [[ -d "${SRCDIR}" ]]
+then
+    echo "Weird error!" >&2
+    exit 1
+fi
+
+if [[ ! -d "${DEST}" ]]
+then
+    mkdir -p "${DEST}"
+fi
+
+rsync -avP --exclude-from="${SRCDIR}/.rsync_excludes.txt" "${SRCDIR}" "${DEST}"
+
+
+# vim:ft=sh
